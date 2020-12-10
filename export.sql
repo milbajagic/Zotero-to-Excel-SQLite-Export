dcb1c5695f1f205952af9b24761fd3cbbd849829
Milica Bajagic, Ph.D <43002114+milbajagic@users.noreply.github.com>
2020-12-10 14:28:58 +0100

    tag1 exclude
    selects only publications with "exclude" in tag1 position

--- export.sql
+++ export.sql
@@ -2,15 +2,15 @@
 /* Basic SELECT statement. */
 SELECT
 /*Identify which fields to SELECT for your output. The left identifier represents the database abbreviated name (set below in the FROM and JOIN statements) and the database field name we want, while the right identifier (after the AS statement) represents the column header as it will appear in your output. */
-i.itemID AS ITEMID,
+-- i.itemID AS ITEMID, 
 title.value AS TITLE,
-t.typeName AS TYPE,
-d.value AS DATE,
-issn.value AS ISSN,
-isbn.value AS ISBN,
-doi.value AS DOI,
-url.value AS URL,
-pub.value AS PUBLICATION_TITLE,
+-- t.typeName AS TYPE,
+-- d.value AS DATE,
+-- issn.value AS ISSN,
+-- isbn.value AS ISBN,
+-- doi.value AS DOI,
+-- url.value AS URL, 
+-- pub.value AS PUBLICATION_TITLE, 
 journalAbbreviation.value AS JOURNAL_ABBREVIATION,
 issue.value AS ISSUE,
 volume.value AS VOLUME,
@@ -18,11 +18,11 @@ series.value AS SERIES,
 pages.value AS PAGES,
 publisher.value AS PUBLISHER,
 place.value AS PLACE,
-proceedingsTitle.value AS PROCEEDINGS,
-bookTitle.value AS BOOK,
-university.value AS UNIVERSITY,
-archiveName.value AS ARCHIVE_NAME,
-archiveLocation.value AS ARCHIVE_LOCATION,
+-- proceedingsTitle.value AS PROCEEDINGS,
+-- bookTitle.value AS BOOK,
+-- university.value AS UNIVERSITY,
+-- archiveName.value AS ARCHIVE_NAME,
+-- archiveLocation.value AS ARCHIVE_LOCATION,
 abstract.value AS ABSTRACT,
 c1.firstName AS AUTHOR_1_FIRST,
 c1.lastName AS AUTHOR_1_LAST,
@@ -42,13 +42,13 @@ ct5.creatorType AS AUTHOR_5_TYPE,
 t1.name AS TAG_1,
 t2.name AS TAG_2,
 t3.name AS TAG_3,
-t4.name AS TAG_4,
-i.dateAdded AS DATE_ADDED,
-i.dateModified AS DATE_MODIFIED,
-g.name AS LIBRARY_NAME,
-i.libraryID AS LIBRARY_ID,
-i.key AS ZOTERO_KEY,
-extra.value AS EXTRA
+t4.name AS TAG_4
+-- i.dateAdded AS DATE_ADDED,
+-- i.dateModified AS DATE_MODIFIED,
+-- g.name AS LIBRARY_NAME,
+-- i.libraryID AS LIBRARY_ID,
+-- i.key AS ZOTERO_KEY,
+--extra.value AS EXTRA 
 /* The general FROM statement merely queries the `items` table. This table does not have much information in it, so all the information we want will be added with JOIN commands below. */
 FROM
 items i
@@ -66,25 +66,65 @@ itemTypes t
 ON
 t.itemTypeID = i.itemTypeID
 LEFT JOIN
-itemDataValues issn
+creators c1
 ON
-issn.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'ISSN' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+c1.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 0,1) LIMIT 1)
 LEFT JOIN
-itemDataValues doi
+creatorTypes ct1
 ON
-doi.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'DOI' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+ct1.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 0,1)
 LEFT JOIN
-itemDataValues isbn
+creators c2
 ON
-isbn.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'ISBN' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+c2.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 1,1) LIMIT 1)
 LEFT JOIN
-itemDataValues url
+creatorTypes ct2
 ON
-url.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'url' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+ct2.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 1,1)
 LEFT JOIN
-itemDataValues pub
+creators c3
 ON
-pub.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'publicationTitle' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+c3.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 2,1) LIMIT 1)
+LEFT JOIN
+creatorTypes ct3
+ON
+ct3.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 2,1)
+LEFT JOIN
+creators c4
+ON
+c4.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 3,1) LIMIT 1)
+LEFT JOIN
+creatorTypes ct4
+ON
+ct4.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 3,1)
+LEFT JOIN
+creators c5
+ON
+c5.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 4,1) LIMIT 1)
+LEFT JOIN
+creatorTypes ct5
+ON
+ct5.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 4,1)
+-- LEFT JOIN
+-- itemDataValues issn
+-- ON
+-- issn.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'ISSN' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+-- LEFT JOIN
+-- itemDataValues doi
+-- ON
+-- doi.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'DOI' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+-- LEFT JOIN
+-- itemDataValues isbn
+-- ON
+-- isbn.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'ISBN' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
+-- LEFT JOIN
+-- itemDataValues url
+-- ON
+-- url.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'url' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1) */
+-- LEFT JOIN
+-- itemDataValues pub
+-- ON
+-- pub.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'publicationTitle' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
 LEFT JOIN
 itemDataValues journalAbbreviation
 ON
@@ -142,46 +182,6 @@ itemDataValues archiveLocation
 ON
 archiveLocation.valueID = (SELECT itemData.valueID FROM itemData WHERE itemData.fieldID = (SELECT fieldID FROM fields WHERE fields.fieldName = 'archiveLocation' LIMIT 1) AND itemData.itemID=i.itemID LIMIT 1)
 LEFT JOIN
-creators c1
-ON
-c1.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 0,1) LIMIT 1)
-LEFT JOIN
-creatorTypes ct1
-ON
-ct1.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 0,1)
-LEFT JOIN
-creators c2
-ON
-c2.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 1,1) LIMIT 1)
-LEFT JOIN
-creatorTypes ct2
-ON
-ct2.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 1,1)
-LEFT JOIN
-creators c3
-ON
-c3.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 2,1) LIMIT 1)
-LEFT JOIN
-creatorTypes ct3
-ON
-ct3.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 2,1)
-LEFT JOIN
-creators c4
-ON
-c4.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 3,1) LIMIT 1)
-LEFT JOIN
-creatorTypes ct4
-ON
-ct4.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 3,1)
-LEFT JOIN
-creators c5
-ON
-c5.creatorID = (SELECT creatorID FROM creators WHERE creatorID = (SELECT creatorID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 4,1) LIMIT 1)
-LEFT JOIN
-creatorTypes ct5
-ON
-ct5.creatorTypeID = (SELECT creatorTypeID FROM itemCreators WHERE itemID = i.itemID ORDER BY orderIndex LIMIT 4,1)
-LEFT JOIN
 tags t1
 ON
 t1.tagID = (SELECT tagID FROM itemTags WHERE itemID = i.itemID LIMIT 1)
@@ -211,14 +211,14 @@ g.libraryID = i.libraryID
 /* End JOIN statements. */
 /* Optional JOIN Statements. */
 /* This is an optional JOIN statement to select for a specific collection. To only show a specific collection in your output, remove the double-hyphen from the beginning of the statement and replace the collectionName value (e.g. "Instructional Design") with the name of your desired collection (encased by quotes). */
---INNER JOIN collectionItems ON collectionItems.itemID = i.itemID AND collectionItems.collectionID = (SELECT collectionID FROM collections WHERE collectionName = "Instructional Design" LIMIT 0,1)
+-- INNER JOIN collectionItems ON collectionItems.itemID = i.itemID AND collectionItems.collectionID = (SELECT collectionID FROM collections WHERE collectionName = "Citation" LIMIT 0,1)
 /* End optional JOIN statements. */
 /* Final conditional WHERE statement allows us to ignore entries that have been deleted in Zotero. */
 WHERE deletedItems.itemID IS NULL
 /* If you would like to add any additional selection criteria, this is the place to do it. Here are some examples that you may wish to use. Merely remove the double-hyphen from the beginning of a condition to make it active in the query. */
 /* Begin optional conditions. */
-/* Only show a specific library. */
---AND g.name = "UTexas IT Students Public Group"
+/* Only show entries with exclude tag in first position */
+AND t1.name = "exclude"
 /* Only show journal articles. */
 --AND t.typeName = "journalArticle"
 /* Only show items where the first author's last name is Smith. */
